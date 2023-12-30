0b0dc263fb9928ec1741b9d115aa21e4f4804e2b
Shawn Hagler <haglershawn@gmail.com>
2023-12-30 15:28:30 -0600

    remove target/

--- shared/database/target/debug/deps/database-8af9dfe1236cf4f3.d
+++
@@ -1,7 +0,0 @@
-/home/admin/devel/d4_theory/shared/database/target/debug/deps/libdatabase-8af9dfe1236cf4f3.rmeta: src/lib.rs
-
-/home/admin/devel/d4_theory/shared/database/target/debug/deps/libdatabase-8af9dfe1236cf4f3.rlib: src/lib.rs
-
-/home/admin/devel/d4_theory/shared/database/target/debug/deps/database-8af9dfe1236cf4f3.d: src/lib.rs
-
-src/lib.rs:
--- shared/database/target/debug/libdatabase.d
+++
@@ -1,1 +0,0 @@
-/home/admin/devel/d4_theory/shared/database/target/debug/libdatabase.rlib: /home/admin/devel/d4_theory/shared/database/src/lib.rs
